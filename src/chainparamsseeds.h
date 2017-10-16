444a796ab28c2bd599fb04636fda13f568ed9a51
likecoin-dev <itcoin@ro.ru>
2017-10-16 06:51:30 +0300

    Update chainparamsseeds.h

--- src/chainparamsseeds.h
+++ src/chainparamsseeds.h
@@ -3,7 +3,7 @@
 
 static const unsigned int pnSeed[] =
 {
-    0x05fab2da,
+//    0x05fab2da,
 //    0x05fab2db,
 //    0x68a766cc,
 //    0x68a766ce,
@@ -11,7 +11,7 @@ static const unsigned int pnSeed[] =
 
 static const unsigned int pnTestnetSeed[] =
 {
-    0x05fab2da,
+//    0x05fab2da,
 //    0x05fab2db,
 //    0x68a766cc,
 //   0x68a766ce,
